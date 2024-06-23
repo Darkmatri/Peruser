9ec5530b15b45e203d5a19e3b182413092d1696d
Darkmatri <kg707574@gmail.com>
2024-06-23 19:40:14 +0530

    search

--- books.js
+++ books.js
@@ -1,8 +1,9 @@
-let grid = document.querySelector(".wrapper");
 let fliterinput = document.getElementById("searchbar");
+let http = new XMLHttpRequest();
+let grid = document.querySelector(".cards");
 
 // fetch('https://freetestapi.com/api/v1/books')
-fetch('/list.json')
+fetch("./list.json")
     .then(res => res.json())
     .then(json => {
         for (let value of json) {
@@ -20,7 +21,7 @@ function fliterproducts() {
     for (let i = 0; i < item.length; i++) {
         let span = item[i].querySelector(".title");
 
-        if (span.innerHTML.toUpperCase().indexOf(filtervalue)>-1) {
+        if (span.innerHTML.toUpperCase().indexOf(filtervalue) > -1) {
             item[i].style.display = "initial";
         } else {
             item[i].style.display = "none";
@@ -31,35 +32,35 @@ function fliterproducts() {
     // console.log(filtervalue);
 }
 
-function addElement(appendin, value) {
-    let div = document.createElement('div');
-    div.className = "books";
+http.open('get', 'list.json', true);
 
-    let {
-        thumbnail,
-        title,
-        authors,
-        categories
-    } = value
+http.send();
 
-    div.innerHTML = `
-    <div class="books">
-        <div class="details">
-            <img src=${imagelinks.thumbnail} alt="" class="cover">
-            <h2 class="title">${title}</h2>
-            <h5 class="author">${authors}</h5>
-            <h3 class="gener">genre: ${categories}</h3>
-            <h3>$200 <span id="price">400</span></h3>
-            <div class="review">
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star-half"></i>
+http.onload = function(){
+    if(this.readyState == 4 && this.status == 200){
+        let products = JSON.parse(this.responseText);
+
+        let out = "";
+        for(let item of products){
+            out += `
+            <div class="books">
+                <div class="details">
+                    <img src="${item.image}" alt="" class="cover">
+                    <h2 class="title">${item.name}</h2>
+                    <h5 class="author">${item.author}</h5>
+                    <h3 class="gener">${item.category}</h3>
+                    <h3>${item.price}<span id="price">${item.span}</span></h3>
+                    <div class="review">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star-half"></i>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    `
-
-    appendin.appendChild(div);
+            `;
+        }
+        document.querySelector(".cards").innerHTML = out;
+    }
 }
